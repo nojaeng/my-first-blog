e07d13b855a36464ecc46badb50235e0a3956052
goove38 <groove38@naver.com>
2019-08-20 22:40:47 +0900

    Added template for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-]
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # post/<int:pk>/ : URL 패턴을 의미
+    # post/ : URL이 post문자를 포함해야 한다
+    # <int:pk> : pk라는 변수로 뷰로 전송    
+ ]
